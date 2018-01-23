ad0bc33f0d10062abbf9e8db019f11d17b6334b3
Lauren Kleckner <klecknerlauren@yahoo.com>
2018-01-23 17:52:19 -0600

    intital

---
+++ crc.c
@@ -0,0 +1,258 @@
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "interface.h"
+
+
+/*
+ * TODO: IMPLEMENT BELOW THREE FUNCTIONS
+ */
+int connect_to(const char *host, const int port);
+struct Reply process_command(const int sockfd, char* command);
+void process_chatmode(const char* host, const int port);
+
+int main(int argc, char** argv) 
+{
+	if (argc != 3) {
+		fprintf(stderr,
+				"usage: enter host address and port number\n");
+		exit(1);
+	}
+
+    display_title();
+    
+	while (1) {
+	
+		int sockfd = connect_to(argv[1], atoi(argv[2]));
+    
+		char command[MAX_DATA];
+        get_command(command, MAX_DATA);
+
+		struct Reply reply = process_command(sockfd, command);
+		display_reply(command, reply);
+		
+		touppercase(command, strlen(command) - 1);
+		if (strncmp(command, "JOIN", 4) == 0) {
+			printf("Now you are in the chatmode\n");
+			process_chatmode(argv[1], reply.port);
+		}
+	
+		close(sockfd);
+    }
+
+    return 0;
+}
+
+/*
+ * Connect to the server using given host and port information
+ *
+ * @parameter host    host address given by command line argument
+ * @parameter port    port given by command line argument
+ * 
+ * @return socket fildescriptor
+ */
+int connect_to(const char *host, const int port)
+{
+	// ------------------------------------------------------------
+	// GUIDE :
+	// In this function, you are suppose to connect to the server.
+	// After connection is established, you are ready to send or
+	// receive the message to/from the server.
+	// 
+	// Finally, you should return the socket fildescriptor
+	// so that other functions such as "process_command" can use it
+	// ------------------------------------------------------------
+
+    // below is just dummy code for compilation, remove it.
+	sockaddr_in addr = *(new sockaddr_in());
+	int sfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sfd!=-1){
+		addr.sin_family = AF_INET;
+		addr.sin_port= htons(port);
+		addr.sin_addr.s_addr = host;
+		memset(&addr, 0, sizeof(addr));
+		if(connect(sfd, (sockaddr*)&addr, sizeof(addr)) == -1){
+			cout<<"Unable to establish connection"<<endl;
+		}
+	}
+	else{
+		cout<<"Error opening socket"<<endl;
+	}
+	
+	return sfd;
+}
+
+/* 
+ * Send an input command to the server and return the result
+ *
+ * @parameter sockfd   socket file descriptor to commnunicate
+ *                     with the server
+ * @parameter command  command will be sent to the server
+ *
+ * @return    Reply    
+ */
+struct Reply process_command(const int sockfd, char* command)
+{
+	// ------------------------------------------------------------
+	// GUIDE 1:
+	// In this function, you are supposed to parse a given command
+	// and create your own message in order to communicate with
+	// the server. Surely, you can use the input command without
+	// any changes if your server understand it. The given command
+    // will be one of the followings:
+	//
+	// CREATE <name>
+	// DELETE <name>
+	// JOIN <name>
+    // LIST
+	//
+	// -  "<name>" is a chatroom name that you want to create, delete,
+	// or join.
+	// 
+	// - CREATE/DELETE/JOIN and "<name>" are separated by one space.
+	// ------------------------------------------------------------
+	char* chatroom;
+	touppercase(command, strlen(command) - 1);
+	if (strncmp(command, "CREATE", 6) == 0){
+		//chatroom = ;
+		Reply.status = SUCCESS;
+		for(int i=0; i<Reply.list_room.size(); i++){
+			if(strcmp(chatroom, Reply.list_room[i]) == 0){
+				Reply.status = FAILURE_ALREADY_EXISTS;
+			}
+		}
+		if(Reply.status == SUCCESS){
+			Reply.list_room.[Reply.list_room.end()-1] = chatroom;
+		}
+	}
+	else if (strncmp(command, "DELETE", 6) == 0){
+		Reply.status = FAILURE_NOT_EXISTS;
+		for(int i=0; i<Reply.list_room.size(); i++){
+			if(strcmp(chatroom, Reply.list_room[i]) == 0){
+				Reply.list_room[i]=NULL;
+				Reply.status = SUCCESS;
+			}
+		}
+	}
+	else if (strncmp(command, "JOIN", 4) == 0){
+		Reply.status = FAILURE_NOT_EXISTS;
+		for(int i=0; i<Reply.list_room.size(); i++){
+			if(strcmp(chatroom, Reply.list_room[i]) == 0){
+				Reply.port= sockfd;
+				Reply.num_member+=1;
+				Reply.status = SUCCESS;
+			}
+		}
+	}
+	else if (strncmp(command, "LIST", 4) == 0){
+		
+	}
+	else{
+		Reply.status = FAILURE_INVALID;
+		//display_title();
+	}
+	
+	display_reply(command, Reply);
+	//Struct Reply 
+	// ------------------------------------------------------------
+	// GUIDE 2:
+	// After you create the message, you need to send it to the
+	// server and receive a result from the server.
+	// ------------------------------------------------------------
+
+
+	// ------------------------------------------------------------
+	// GUIDE 3:
+	// Then, you should create a variable of Reply structure
+	// provided by the interface and initialize it according to
+	// the result.
+	//
+	// For example, if a given command is "JOIN room1"
+	// and the server successfully created the chatroom,
+	// the server will reply a message including information about
+	// success/failure, the number of members and port number.
+	// By using this information, you should set the Reply variable.
+	// the variable will be set as following:
+	//
+	// Reply reply;
+	// reply.status = SUCCESS;
+	// reply.num_member = number;
+	// reply.port = port;
+	// 
+	// "number" and "port" variables are just an integer variable
+	// and can be initialized using the message fomr the server.
+	//
+	// For another example, if a given command is "CREATE room1"
+	// and the server failed to create the chatroom becuase it
+	// already exists, the Reply varible will be set as following:
+	//
+	// Reply reply;
+	// reply.status = FAILURE_ALREADY_EXISTS;
+    // 
+    // For the "LIST" command,
+    // You are suppose to copy the list of chatroom to the list_room
+    // variable. Each room name should be seperated by comma ','.
+    // For example, if given command is "LIST", the Reply variable
+    // will be set as following.
+    //
+    // Reply reply;
+    // reply.status = SUCCESS;
+    // strcpy(reply.list_room, list);
+    // 
+    // "list" is a string that contains a list of chat rooms such 
+    // as "r1,r2,r3,"
+	// ------------------------------------------------------------
+
+	// REMOVE below code and write your own Reply.
+	/*struct Reply reply;
+	reply.status = SUCCESS;
+	reply.num_member = 5;
+	reply.port = 1024;
+	return reply;*/
+}
+
+/* 
+ * Get into the chat mode
+ * 
+ * @parameter host     host address
+ * @parameter port     port
+ */
+void process_chatmode(const char* host, const int port)
+{
+	// ------------------------------------------------------------
+	// GUIDE 1:
+	// In order to join the chatroom, you are supposed to connect
+	// to the server using host and port.
+	// You may re-use the function "connect_to".
+	// ------------------------------------------------------------
+
+	// ------------------------------------------------------------
+	// GUIDE 2:
+	// Once the client have been connected to the server, we need
+	// to get a message from the user and send it to server.
+	// At the same time, the client should wait for a message from
+	// the server.
+	// ------------------------------------------------------------
+	
+    // ------------------------------------------------------------
+    // IMPORTANT NOTICE:
+    // 1. To get a message from a user, you should use a function
+    // "void get_message(char*, int);" in the interface.h file
+    // 
+    // 2. To print the messages from other members, you should use
+    // the function "void display_message(char*)" in the interface.h
+    //
+    // 3. Once a user entered to one of chatrooms, there is no way
+    //    to command mode where the user  enter other commands
+    //    such as CREATE,DELETE,LIST.
+    //    Don't have to worry about this situation, and you can 
+    //    terminate the client program by pressing CTRL-C (SIGINT)
+	// ------------------------------------------------------------
+}
+
